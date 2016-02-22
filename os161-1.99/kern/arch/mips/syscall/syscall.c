55bc3dced2e270fe9e066d05ba82bd6dd3692c38
AmyAmyZhu <zhuzefang12@gmail.com>
2016-02-21 23:27:09 -0500

    case fork

--- os161-1.99/kern/arch/mips/syscall/syscall.c
+++ os161-1.99/kern/arch/mips/syscall/syscall.c
@@ -132,7 +132,11 @@ syscall(struct trapframe *tf)
 #endif // UW
 
 	    /* Add stuff here */
- 
+#if OPT_A2
+    case SYS_fork:
+        err = sys_fork(tf, (pid_t *)&retval);
+        break;
+#endif // OPT_A2
 	default:
 	  kprintf("Unknown syscall %d\n", callno);
 	  err = ENOSYS;
