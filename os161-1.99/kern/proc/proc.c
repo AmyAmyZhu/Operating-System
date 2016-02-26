298e3ec970cca0d6ab1aab7c0a83c931dd14d785
AmyAmyZhu <zhuzefang12@gmail.com>
2016-02-25 23:55:27 -0500

    test

--- os161-1.99/kern/proc/proc.c
+++ os161-1.99/kern/proc/proc.c
@@ -158,7 +158,7 @@ void proc_exit(struct proc *p, int exitcode){
     
     if(get_parent_pid(p) == -1){
         //kprintf("Here!!come remove proctree\n");
-        //remove_proctree(p);
+        remove_proctree(p);
         //kprintf("Here!!leave remove proctree\n");
     } else {
         //cv_signal(p->wait, proc_lock);
