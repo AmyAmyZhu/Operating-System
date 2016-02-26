aaff20f247a6bacea6a67e9ed293c55b6ca80c51
AmyAmyZhu <zhuzefang12@gmail.com>
2016-02-26 00:41:11 -0500

    test

--- os161-1.99/kern/proc/proc.c
+++ os161-1.99/kern/proc/proc.c
@@ -341,25 +341,25 @@ proc_destroy(struct proc *proc)
     //}
 #endif // UW
     
-    threadarray_cleanup(&proc->p_threads);
-    spinlock_cleanup(&proc->p_lock);
+    //threadarray_cleanup(&proc->p_threads);
+    //spinlock_cleanup(&proc->p_lock);
     
-    kfree(proc->p_name);
-    kfree(proc);
+    //kfree(proc->p_name);
+    //kfree(proc);
     
 #ifdef UW
     /* decrement the process count */
     /* note: kproc is not included in the process count, but proc_destroy
      is never called on kproc (see KASSERT above), so we're OK to decrement
      the proc_count unconditionally here */
-    //P(proc_count_mutex);
-    //KASSERT(proc_count > 0);
-    //proc_count--;
+    P(proc_count_mutex);
+    KASSERT(proc_count > 0);
+    proc_count--;
     /* signal the kernel menu thread if the process count has reached zero */
-    //if (proc_count == 0) {
-      //  V(no_proc_sem);
-    //}
-    //V(proc_count_mutex);
+    if (proc_count == 0) {
+        V(no_proc_sem);
+    }
+    V(proc_count_mutex);
 #endif // UW
     
     
