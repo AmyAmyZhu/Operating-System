3381bb3ea654cca81164faae99e334c580807eb4
AmyAmyZhu <zhuzefang12@gmail.com>
2016-02-25 21:47:31 -0500

    all my version

--- os161-1.99/kern/arch/mips/syscall/syscall.c
+++ os161-1.99/kern/arch/mips/syscall/syscall.c
@@ -27,6 +27,7 @@
  * SUCH DAMAGE.
  */
 
+#include "opt-A2.h"
 #include <types.h>
 #include <kern/errno.h>
 #include <kern/syscall.h>
@@ -35,7 +36,7 @@
 #include <thread.h>
 #include <current.h>
 #include <syscall.h>
-
+#include <addrspace.h>
 
 /*
  * System call dispatcher.
@@ -132,7 +133,11 @@ syscall(struct trapframe *tf)
 #endif // UW
 
 	    /* Add stuff here */
- 
+#if OPT_A2
+    case SYS_fork:
+        err = sys_fork(tf, (pid_t *)&retval);
+        break;
+#endif // OPT_A2
 	default:
 	  kprintf("Unknown syscall %d\n", callno);
 	  err = ENOSYS;
@@ -176,8 +181,24 @@ syscall(struct trapframe *tf)
  *
  * Thus, you can trash it and do things another way if you prefer.
  */
-void
-enter_forked_process(struct trapframe *tf)
+//#if OPT_A2
+void enter_forked_process(void *argc1, unsigned long argc2)
 {
-	(void)tf;
+    KASSERT(argc2 == 1);
+    // code you created or modified for ASST2 goes here
+    as_activate();
+
+    struct trapframe stack = *((struct trapframe *)argc1);
+    kfree(argc1);
+    
+    stack.tf_a3 = 0;
+    stack.tf_v0 = 0; // no children
+    /*
+     * Now, advance the program counter, to avoid restarting
+     * the syscall over and over again.
+     */
+    stack.tf_epc += 4;
+    
+    mips_usermode(&stack);
 }
+//#endif /* OPT_A2 */
