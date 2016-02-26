21f1eb09feed057a2b0fcd149dac92db7a9deeec
AmyAmyZhu <zhuzefang12@gmail.com>
2016-02-25 21:39:35 -0500

    try 3

--- os161-1.99/kern/proc/proc.c
+++ os161-1.99/kern/proc/proc.c
@@ -79,10 +79,10 @@ int num;
 int limit;
 
 int add_proctree(struct proc *p, struct proc *new){
-    KASSERT(proc_lock != NULL);
+    //KASSERT(proc_lock != NULL);
     KASSERT(p != NULL);
     
-    int change = 0;    
+    int change = 0;
     /*if(num == limit-1){
         if(limit < 256) {
             limit = limit * 2;
