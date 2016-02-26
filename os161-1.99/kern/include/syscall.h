4844c88f55589be972e76d7ca1c27bbd5d86b6c8
AmyAmyZhu <zhuzefang12@gmail.com>
2016-02-26 13:51:56 -0500

    test

--- os161-1.99/kern/include/syscall.h
+++ os161-1.99/kern/include/syscall.h
@@ -30,6 +30,7 @@
 #ifndef _SYSCALL_H_
 #define _SYSCALL_H_
 
+#include "opt-A2.h"
 
 struct trapframe; /* from <machine/trapframe.h> */
 
@@ -44,7 +45,11 @@ void syscall(struct trapframe *tf);
  */
 
 /* Helper for fork(). You write this. */
+#if OPT_A2
+void enter_forked_process(void *argc1, unsigned long argc2);
+#else
 void enter_forked_process(struct trapframe *tf);
+#endif // OPT_A2
 
 /* Enter user mode. Does not return. */
 void enter_new_process(int argc, userptr_t argv, vaddr_t stackptr,
@@ -65,5 +70,9 @@ int sys_getpid(pid_t *retval);
 int sys_waitpid(pid_t pid, userptr_t status, int options, pid_t *retval);
 
 #endif // UW
+
+#if OPT_A2
+int sys_fork(struct trapframe *tf, pid_t *retval);
+#endif // OPT_A2
 
 #endif /* _SYSCALL_H_ */
