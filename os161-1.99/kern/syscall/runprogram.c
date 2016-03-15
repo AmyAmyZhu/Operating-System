5eb4411eaf9ccca0ef49eadf0087d8f9deecdd68
AmyAmyZhu <zhuzefang12@gmail.com>
2016-03-15 19:09:41 -0400

    fix

--- os161-1.99/kern/syscall/runprogram.c
+++ os161-1.99/kern/syscall/runprogram.c
@@ -109,6 +109,7 @@ runprogram(char *progname)
     
 #if OPT_A2
     // copy argv to the stack of user address space
+    int offset;
     char** addr_ptr = kmalloc((argc+1)*sizeof(char*)); // sizeof(char*) = 4 bytes
     for (int i=argc-1; i>=0; --i){
         char* arg_str = argv[i];
